b5f5db46d5c02f2a8c36c8c9f86ddbab378bc845
Vikram Talware <vikram.talware@thq-m-vtalw01.lan>
2020-04-12 12:01:52 -0700

    commits

---
+++ ex3.py
@@ -0,0 +1,8 @@
+# Order of Operations PEMDAS - Parentheses (simplify inside 'em), Exponents, Multiplication and Division (from left to right), Addition and Subtraction (from left to right)
+
+print ("I will now count my chickens:")
+
+print ("Hens", 25 + 30 / 6)
+print ("Roosters", 100 - 25 * 3 % 4)
+
+print ("Now I will count the eggs:")
