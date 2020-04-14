2ee5321a80937d3088438b2b01e306257ded591d
Vikram Talware <vikram.talware@thq-m-vtalw01.corp.lookout.com>
2020-04-13 17:55:45 -0700

    added more code

--- ex3.py
+++ ex3.py
@@ -1,9 +1,24 @@
-'''Order of Operations PEMDAS - Parentheses (simplify inside 'em), Exponents, Multiplication and Division (from left to right),
+'''Order of Operations PEMDAS - Parentheses (simplify inside 'em), Exponents, Multiplication, Modulus and Division (from left to right),
 Addition and Subtraction (from left to right)'''
 
 print ("I will now count my chickens:")
 
-print ("Hens", 25 + 30 / 6)
+print ("Hens", int(25 + 30 / 6))
 print ("Roosters", 100 - 25 * 3 % 4)
 
 print ("Now I will count the eggs:")
+print (round(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+print ("Is it true that 3 + 2 < 5 - 7?")
+print (3 + 2 < 5 - 7)
+
+print ("What is 3 + 2?", 3 + 2)
+print ("What is 5 - 7?", 5 - 7)
+
+print ("Oh, that's why it's False.")
+
+print ("How about some more.")
+
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
