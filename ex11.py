cd78d49adcd072abfbe568b2b9f6a1ea9547550c
Vikram Talware <vikram.talware@thq-m-vtalw01.lan>
2020-05-02 19:22:33 -0700

    exercise 11 done

---
+++ ex11.py
@@ -0,0 +1,14 @@
+print ("How old are you?"),
+age = input()
+print ("How tall are you?"),
+height = input()
+print ("How much do you weigh?"),
+weight = input()
+
+print ("So, you're %r old, %s tall and %r heavy." %(age,height,weight))
+
+
+# The input() function allows user input.
+
+x = input('Enter your name:')
+print('Hello, ' + x)
